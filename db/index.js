ed4b297c30b74731cfd74aaec29d417c111b950b
cazmaar <kazeem.alatishe@yahoo.com>
2021-12-25 13:22:39 +0000

    table created in database

--- db/index.js
+++ db/index.js
@@ -1,17 +1,3 @@
-// Host
-// ec2-52-208-221-89.eu-west-1.compute.amazonaws.com
-// Database
-// d1ivdvqjgn2p01
-// User
-// ifvuckaqbcgjsh
-// Port
-// 5432
-// Password
-// e35e07ad45ca44453a49a58e8f8670e6adfa6826bea5549caa265d9a38a7f4e6
-// URI
-// postgres://ifvuckaqbcgjsh:e35e07ad45ca44453a49a58e8f8670e6adfa6826bea5549caa265d9a38a7f4e6@ec2-52-208-221-89.eu-west-1.compute.amazonaws.com:5432/d1ivdvqjgn2p01
-// Heroku CLI
-// heroku pg:psql postgresql-amorphous-84014 --app kazeemapp
 import pg from "pg";
 import * as db from "../config.js";
 
@@ -22,9 +8,10 @@ const pool = new pg.Pool({
   database: db.database,
   password: db.password,
   port: db.port,
+  ssl: { rejectUnauthorized: false },
 });
 
 // function created to query the database
-async function query(text, params) {
+export default async function query(text, params) {
   return pool.query(text, params);
 }
