48bbd89c9f1b1e247f214eec196b4e27020e1129
cazmaar <kazeem.alatishe@yahoo.com>
2021-12-31 10:22:36 +0000

    html structure footer deleted

--- public/js/main.js
+++ public/js/main.js
@@ -11,7 +11,7 @@ const months = document.querySelector("#months");
 const getListByLocation = document.querySelector("#restaurantlocation");
 const restaurantRating = document.querySelector("#restaurant-rating");
 
-// // fetches all the restaurant data from the API
+// fetches all the restaurant data from the API
 async function getter() {
   const response = await fetch("http://localhost:3000/restaurants");
   const data = await response.json();
