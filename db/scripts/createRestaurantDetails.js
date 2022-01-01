4a71e1f7d5f314fcfb20a049659509f488e2b12b
cazmaar <kazeem.alatishe@yahoo.com>
2022-01-01 22:41:25 +0000

    table reoppulated

--- db/scripts/createRestaurantDetails.js
+++ db/scripts/createRestaurantDetails.js
@@ -1,7 +1,7 @@
 import query from "../index.js";
 
 const sqlString =
-  "CREATE TABLE IF NOT EXISTS restaurants_details (id SERIAL PRIMARY KEY, date DATE, restaurant_name TEXT, location TEXT, year INT, month TEXT)";
+  "CREATE TABLE IF NOT EXISTS restaurants_details (id SERIAL PRIMARY KEY, date DATE, restaurant_name TEXT, location TEXT, month TEXT)";
 
 // function to create table is postgreSQL
 const createRestaurantDetails = async () => {
