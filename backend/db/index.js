4583817da14275c68b5628d4c82a9ac1b8e43a5a
cazmaar <kazeem.alatishe@yahoo.com>
2022-02-14 23:26:48 +0000

    Env added

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,10 +1,12 @@
 import pg from "pg";
 import { dbTest, dbProd, Env } from "../config.js";
+
+
 // database connection created with the use of pool
-// let enviroment = "";
-// Env === "test" ? (enviroment = dbTest) : (enviroment = dbProd);
-// console.log(enviroment)
-const pool = new pg.Pool(dbProd);
+ let enviroment = "";
+ Env === "test" ? (enviroment = dbTest) : (enviroment = dbProd);
+
+const pool = new pg.Pool(enviroment);
 
 // function created to query the database
 export default async function query(text, params) {
