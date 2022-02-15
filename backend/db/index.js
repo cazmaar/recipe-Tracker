75b785de1118fc754dfd36490ec44f8e40863601
cazmaar <kazeem.alatishe@yahoo.com>
2022-02-15 09:34:14 +0000

    Database url changed

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,12 +1,15 @@
 import pg from "pg";
-import { dbTest, dbProd, Env } from "../config.js";
+// import { dbTest, dbProd, Env } from "../config.js";
 
 
 // database connection created with the use of pool
- let enviroment = "";
- Env === "test" ? (enviroment = dbTest) : (enviroment = dbProd);
+//  let enviroment = "";
+//  Env === "test" ? (enviroment = dbTest) : (enviroment = dbProd);
 
-const pool = new pg.Pool(enviroment);
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
 
 // function created to query the database
 export default async function query(text, params) {
